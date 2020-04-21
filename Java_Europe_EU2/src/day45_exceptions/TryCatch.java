b925abe3d8a635211112d050e89d7011e6bdf77d
europe <europe@192.168.1.153>
2020-04-21 13:10:41 -0400

    EU2

--- Java_Europe_EU2/src/day45_exceptions/TryCatch.java
+++ Java_Europe_EU2/src/day45_exceptions/TryCatch.java
@@ -9,17 +9,20 @@ public class TryCatch {
 			
 			String str = "JAVA";
 			System.out.println(str.charAt(0));
-			System.out.println(str.charAt(2));  //i might have problem here 
-			System.out.println(str.charAt(1));  //WILL NOT BE EXECUTED
+			System.out.println(str.charAt(2));  //GETTING data from 3rd party
+			System.out.println(str.charAt(1));  //using data here
+			//use data 
 			
 		}catch(Exception e) {
 			
 			System.out.println("Exception happened in try block and was cought");
 			//EXCEL
+
 		}
 		
 		System.out.println("After try catch block");
 		
+	
 		
 		
 		
